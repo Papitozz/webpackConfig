a4953d7833eb9c00bb0e3dbca33bdda5e764abca
s1re <papitozzone@gmail.com>
2024-08-29 18:25:27 +0700

    new version

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
@@ -12,38 +12,34 @@ module.exports = {
         slider: './src/js/slider.js', // Точка входа для файла JavaScript слайдера
     },
     output: {
-        path: path.resolve(__dirname, 'dist'), // Путь для выходных файлов (скриптов и стилей)
-        filename: 'js/[name].bundle.js', // Путь и имя выходных JavaScript файлов
+        path: path.resolve(__dirname, 'dist/js'), // Путь для выходных файлов (скриптов)
     },
     devtool: 'source-map', // Включаем создание source maps для отладки
     module: {
         rules: [
             {
                 test: /\.scss$/, // Правило для обработки файлов .scss
-                use: [
-                    MiniCssExtractPlugin.loader, // Извлечение CSS в отдельный файл
-                    'css-loader', // Загрузка CSS
-                    'sass-loader' // Компиляция SCSS в CSS
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] // Лоадеры для компиляции SCSS в CSS и вставки стилей в DOM
             }
         ]
     },
     plugins: [
         // Плагин для извлечения CSS в отдельные файлы
         new MiniCssExtractPlugin({
-            filename: 'css/[name].css', // Имя выходных CSS файлов
+            filename: '../css/[name].css', // Имя выходных CSS файлов
         }),
         // Плагин для копирования файлов (например, изображений) в выходную директорию
         new CopyWebpackPlugin({
             patterns: [
-                {from: 'src/assets/img/', to: 'img/'} // Копируем файлы из src/assets/img/ в dist/img/
+                {from: 'src/assets/img/', to: '../img/'}, // Копируем файлы из src/assets/img/ в dist/img/
+                {from: 'src/assets/server.php', to: '../'}
             ]
         }),
         // Плагин для генерации HTML-файла на основе шаблона
         new HtmlWebpackPlugin({
             filename: '../index.html', // Имя выходного HTML-файла
             template: 'src/index.html', // Путь к исходному HTML-шаблону
-            inject: true, // Включаем автоматическое добавление <script> и <link> тегов
+            inject: false, // Отключаем автоматическое добавление <script> и <link> тегов
         }),
         // Плагин для минимизации изображений
         new ImageMinimizerPlugin({
@@ -65,20 +61,13 @@ module.exports = {
               },
             },
           }),
-        // Плагин для минимизации CSS
-        new CssMinimizerPlugin(),
+          new CssMinimizerPlugin(),
     ],
-    optimization: {
-        minimize: true, // Включаем минимизацию
-        minimizer: [
-            '...', // Используем встроенные минимизаторы (например, TerserPlugin для JavaScript)
-            new CssMinimizerPlugin(), // Добавляем плагин для минимизации CSS
-        ],
-    },
+    
     watch: true, // Включаем вотчер для автоматической пересборки при изменениях
     watchOptions: {
         ignored: /node_modules/, // Исключаем папку node_modules из отслеживания
         aggregateTimeout: 300,   // Задержка перед пересборкой после изменений (мс)
         poll: 1000, // Частота опроса изменений (мс)
     }
-};
+}
